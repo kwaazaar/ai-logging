543c2623c7e3d990d816f965dc21de803c030e19
Robert te Kaat <hvkaat@aahg.onmicrosoft.com>
2019-04-10 10:00:17 +0200

    Add project files.

---
+++ AI-Demo-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AI_Demo_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
